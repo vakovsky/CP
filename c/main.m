3c8d55a1d38092b062f07182d54dae54069ff005
Dimiter Vakovsky <vakovsky@gmail.com>
2025-10-24 19:19:48 +0300

    Update main.m

--- c/main.m
+++ c/main.m
@@ -11,5 +11,41 @@ table = ...
    'ШЩЬЪЮЯ'];
 text = 'ЛЕКЦИИТЕСАВПОНЕДЕЛНИК';
 
+% Потребителски ключ
+validKey = false;
+while ~validKey
+    key = input('Въведи ключ от 6 различни символа: ', 's');
+    
+    if length(key) ~= 6
+        fprintf('Ключът трябва да е точно 6 символа!\n');
+        continue
+    end
+    if length(unique(key)) ~= 6
+        fprintf('Всички символи трябва да са различни!\n');
+        continue
+    end
+    validKey = true;
+end
+
+% Даден низ с азбука/символи за таблицата
+alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЧШЩЬЪЮЯ';  % пример
+
+% Премахваме символите, които са в ключа
+remainingChars = alphabet(~ismember(alphabet, key));
+
+% Проверка, че имаме достатъчно символи
+if length(remainingChars) < 24
+    error('Няма достатъчно символи за останалите редове.');
+end
+
+% Създаваме останалите редове (по 6 символа на ред)
+tableRows = reshape(remainingChars(1:24), 6, 4)';  % 4 реда по 6 символа
+
+% Комбинираме с ключа
+table = [key; tableRows];
+
+disp('Създадена таблица:')
+disp(table)
+
 pairs = pairChars(text)
 newPairs = updateBothSymbols(pairs, table)
